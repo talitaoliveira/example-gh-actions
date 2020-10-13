fafb5dd6ecb062fb2e9adbdc2f2a51a2023119b0
Talita Oliveira <litaa.oliveira@gmail.com>
2020-10-13 13:55:14 -0300

    Add small change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Olá mundo</h1>
-    </div>
+      <h1>Olá mundo!</h1>
   );
 }
 
