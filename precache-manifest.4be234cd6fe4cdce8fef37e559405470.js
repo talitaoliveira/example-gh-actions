7603190ee5abcae7dd837e16a37f6fca46fb7ccc
talitaoliveira <litaa.oliveira@gmail.com>
2020-10-14 01:37:33 +0000

    Deploying to gh-pages from  @ 8b84b509721511203923b5fef6ad7c76c5b785c2 🚀

---
+++ precache-manifest.4be234cd6fe4cdce8fef37e559405470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc56d683382071c0e62e9998df51226",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8aa7d849984df9715e8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ab3e8d6fa4b329a6fad3",
+    "url": "/static/js/2.b1201569.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1201569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8aa7d849984df9715e8",
+    "url": "/static/js/main.f6daa2de.chunk.js"
+  },
+  {
+    "revision": "21008935945395bb28a0",
+    "url": "/static/js/runtime-main.eb018c73.js"
+  }
+]);
